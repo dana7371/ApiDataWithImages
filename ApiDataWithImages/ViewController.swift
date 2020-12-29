2ebcee25c738f176378951637f2f9151ec8bffba
Dana <dana73711@gmail.com>
2020-12-28 19:32:52 -0800

    Initial Commit

---
+++ ApiDataWithImages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApiDataWithImages
+//
+//  Created by Dana Palmer on 12/28/20.
+//  Copyright © 2020 Dana Palmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
